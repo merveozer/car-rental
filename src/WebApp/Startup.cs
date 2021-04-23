9ae8ec75bf82a56797b51b6c485eab944023f9e2
Merve Özer <74708604+merveozer@users.noreply.github.com>
2021-04-23 14:56:50 +0300

    Adding color type service.

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -31,6 +31,7 @@ namespace WebApp
             services.AddScoped<ICarRentalDbContext>(provider => provider.GetService<CarRentalDbContext>());
             services.AddScoped<IVehicleBrandService, VehicleBrandService>();
             services.AddScoped<IVehicleModelService, VehicleModelService>();
+            services.AddScoped<IColorTypeService, ColorTypeService>();
         }
 
         private int VehicleBrandService()
