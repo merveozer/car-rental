8984c351e8be5f62aee7e1971876acfd5d2ceba1
Merve Özer <74708604+merveozer@users.noreply.github.com>
2021-04-23 20:30:43 +0300

    Adding vehicle class type service.

---
+++ src/Application/Services/Concrete/VehicleClassTypeService.cs
@@ -0,0 +1,57 @@
+﻿using Application.Infrastructure.Persistence;
+using Application.Services.Common;
+using Domain.DTOs;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Concrete
+{
+    public class VehicleClassTypeService : BaseService, IVehicleClassTypeService
+    {
+        public VehicleClassTypeService(ICarRentalDbContext context) : base(context)
+        {
+        }
+
+        public Response Add(VehicleClassType vehicleClassType)
+        {
+            Context.VehicleClassType.Add(vehicleClassType);
+            Context.SaveChanges();
+            return Response.Success("Araç tipi başarıyla kaydedildi.");
+        }
+
+        public Response Delete(int id)
+        {
+            var vehicleClassTypeToDelete = GetById(id);
+            Context.VehicleClassType.Remove(vehicleClassTypeToDelete);
+            Context.SaveChanges();
+            return Response.Success("Araç tipi başarıyla silindi.");
+        }
+
+        public List<VehicleClassType> Get(VehicleClassFilter filter)
+        {
+            var items = (from c in Context.VehicleClassType
+                         orderby c.Name
+                         select c).ToList();
+            return items;
+        }
+
+        public VehicleClassType GetById(int id)
+        {
+            return Context.VehicleClassType.Where(c => c.Id == id).SingleOrDefault();
+
+        }
+
+        public Response Update(VehicleClassType vehicleClassType)
+        {
+            var vehicleClassTypeToUpdate = GetById(vehicleClassType.Id);
+            vehicleClassTypeToUpdate.Name = vehicleClassType.Name;
+            Context.SaveChanges();
+
+            return Response.Success("Araç tipi başarıyla güncellendi.");
+        }
+    }
+}
---
+++ src/Application/Services/IVehicleClassTypeService.cs
@@ -0,0 +1,20 @@
+﻿using Domain.DTOs;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public interface IVehicleClassTypeService
+    {
+        Response Add(VehicleClassType vehicleClassType);
+        Response Update(VehicleClassType vehicleClassType);
+        Response Delete(int id);
+        VehicleClassType GetById(int id);
+        List<VehicleClassType> Get(VehicleClassFilter filter);
+
+    }
+}
