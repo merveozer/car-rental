4693de461c31f5b88615e600193fc9657c907452
Merve Özer <74708604+merveozer@users.noreply.github.com>
2021-04-23 16:12:00 +0300

    Adding fuel type service.

---
+++ src/Application/Services/Concrete/FuelTypeService.cs
@@ -0,0 +1,57 @@
+﻿using Application.Infrastructure.Persistence;
+using Application.Services.Common;
+using Domain.DTOs;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Concrete
+{
+    public class FuelTypeService : BaseService, IFuelTypeService
+    {
+        public FuelTypeService(ICarRentalDbContext context) : base(context)
+        {
+
+        }
+
+        public Response Add(FuelType fuelType)
+        {
+            Context.FuelType.Add(fuelType);
+            Context.SaveChanges();
+            return Response.Success("Yakıt tipi başarıyla kaydedildi.");
+        }
+
+        public Response Delete(int id)
+        {
+            var fuelTypeToDelete = GetById(id);
+            Context.FuelType.Remove(fuelTypeToDelete);
+            Context.SaveChanges();
+            return Response.Success("Yakıt tipi başarıyla silindi.");
+        }
+
+        public List<FuelType> Get(FuelTypeFilter filter)
+        {
+            var items = (from f in Context.FuelType
+                         orderby f.Name
+                         select f).ToList();
+            return items;
+        }
+
+        public FuelType GetById(int id)
+        {
+            return Context.FuelType.Where(f => f.Id == id).SingleOrDefault();
+        }
+
+        public Response Update(FuelType fuelType)
+        {
+            var fuelTypeToUpdate = GetById(fuelType.Id);
+            fuelTypeToUpdate.Name = fuelType.Name;
+            Context.SaveChanges();
+
+            return Response.Success("Yakıt tipi başarıyla güncellendi.");
+        }
+    }
+}
