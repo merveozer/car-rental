6291e21dc65febe663c3362e9818364b6f0e0d2c
Merve Özer <74708604+merveozer@users.noreply.github.com>
2021-05-09 10:13:43 +0300

    Completing vehicle detail page.

--- src/Domain/DTOs/VehicleFilter.cs
+++ src/Domain/DTOs/VehicleFilter.cs
@@ -5,13 +5,25 @@ namespace Domain.DTOs
 {
     public class VehicleFilter
     {
-        [Display (Name = "") ]
-        public int? VehicleBrandId { get; set; }
-        public int? VehicleModelId { get; set; }
-        public int TransmissionTypeId { get; } 
+        //[Display (Name = "") ]
+        //public int VehicleBrandId { get; set; }
+
+        [Display(Name = "Araç Modeli")]
+        public int VehicleModelId { get; set; }
+
+        [Display(Name = "Vites Tipi")]
+        public int TransmissionTypeId { get; }
+
+        [Display(Name = "Lastik Tipi")]
         public int TireTypeId { get; set; }
+
+        [Display(Name = "Yakıt Tipi")]
         public int FuelTypeId { get; } = new int();
-        public int VehicleClassTypeId { get; } 
+
+        [Display(Name = "Araç Tipi")]
+        public int VehicleClassTypeId { get; }
+
+        [Display(Name = "Renk")]
         public int ColorTypeId { get; } 
         public RangeValue<int?> ProductionYearRange { get; set; } = new RangeValue<int?>(); //soru işareti olduğundan nullable oldu
         public RangeValue<int?> EngineDisplacementRange { get; set; } = new RangeValue<int?>();
--- src/Domain/DTOs/VehicleRentalPriceFilter.cs
+++ src/Domain/DTOs/VehicleRentalPriceFilter.cs
@@ -8,10 +8,12 @@ namespace Domain.DTOs
 {
     public class VehicleRentalPriceFilter
     {
-        public VehicleRentalPriceFilter(int vehicleId)
+        public VehicleRentalPriceFilter(int vehicleId, DateTime? date= null)
         {
             VehicleId = vehicleId;
+            Date = date;
         }
         public int VehicleId { get; set; }
+        public DateTime? Date { get; set; }
     }
 }
